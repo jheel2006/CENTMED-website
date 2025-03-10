5b96d17f1a6c6d6b5e005e18eda0cb5f28e293ab
jheel2006 <jheelthanki@gmail.com>
2025-03-10 13:42:59 +0400

    added centmed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css"; // Import the CSS file
+import logo from "../assets/CENTMED_logo.png";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             {/* Left side: Logo */}
-            <div className="logo">
-                <Link to="/" className="logo-text">Your Logo</Link> {/* Placeholder Logo */}
+            <div className="centmed-logo">
+                <Link to="/">
+                    <img src={logo} alt="CENTMED Logo" className="logo-image" />
+                </Link>
             </div>
 
             {/* Right side: Navigation Items */}
